eb1401bc24c78d067eaaf78ab29589417ff7d340
wangjing90 <wangjing90@jd.com>
2019-12-22 12:45:05 +0800

    add: 详情页数据渲染

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,66 +1,15 @@
-// pages/posts/post-detail/post-detail.js
+var postsData = require('../../../data/posts-data.js')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    var postId = options.id
+    var postData = postsData.postList[postId]
+    // 如果在onLoad方法中，不是异步的去执行一个数据绑定
+    // 则不需要使用this.setData方法
+    // 只需要对this.data赋值即可
+    // this.data.postData = postData ?
+    this.setData({
+      postData: postData
+    })
   }
 })
